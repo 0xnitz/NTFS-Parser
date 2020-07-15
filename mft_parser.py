d53d5c41797f91bf7b9aacca13e4fe6e9ed6cbb7
prerikoth <nitzan.adut@gmail.com>
2020-07-15 13:56:55 +0300

    Added mft_parser

---
+++ mft_parser.py
@@ -0,0 +1,13 @@
+class MFTParser:
+    def __init__(self):
+        pass
+
+    def get_attribute(self, attribute_code, entry):
+        """
+        This function will receive an attribute code and an mft entry and will return the attribute's data
+        :param attribute_code: attribute code in the mft
+        :param entry: MFTEntry object
+        :return: attribute's data
+        """
+
+        return b''
