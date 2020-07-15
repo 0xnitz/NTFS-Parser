bac8838992e4e4ca8b336e3416fea707c827c681
prerikoth <nitzan.adut@gmail.com>
2020-07-15 15:44:41 +0300

    Adding main and stringing everything together...

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
+from parser import *
+import sys
 
 def main():
-    pass
+    #if len(sys.argv) != 2:
+    #    print('This program will search for a file in file system and print it\'s contents!\n'
+    #          'Usage: {0} filename_to_search'.format(sys.argv[0]))
+    #    exit(1)
+    #
+    #filename = sys.argv[1]
+    filename = '$MFT'
+    parser = Parser()
+    parser.find_file(filename)
 
 
 if __name__ == "__main__":
--- sector_reader.py
+++ sector_reader.py
@@ -6,16 +6,36 @@ class SectorReader:
         self.disk = disk
 
     def read_from(self, sector_start, length=1):
+        """
+        This function reads length sectors from sector_start
+        :param sector_start: The sector to start reading from
+        :param length: How many sectors to read, default is 1
+        :return: Binary data from the sectors
+        """
+
         file = open(self.disk, 'rb')
         file.seek(sector_start * SECTOR_SIZE)
         return file.read(SECTOR_SIZE * length)
 
     def read_sector(self, sector):
+        """
+        Reads a single sector
+        :param sector: Sector to read
+        :return: The sector's binary data
+        """
+
         file = open(self.disk, 'rb')
         file.seek(sector * SECTOR_SIZE)
         return file.read(SECTOR_SIZE)
 
     def read_until(self, sector_start, string):
+        """
+        Read sectors until a string
+        :param sector_start: The sector to start reading from
+        :param string: The string to find
+        :return: The Binary data read
+        """
+
         file = open(self.disk, 'rb')
         file.seek(sector_start * SECTOR_SIZE)
         data = b''
