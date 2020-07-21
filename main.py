192899ae248348368566545fe7e9d2464a9e41a4
prerikoth <nitzan.adut@gmail.com>
2020-07-21 16:59:12 +0300

    Now using argparse

--- main.py
+++ main.py
@@ -1,33 +1,49 @@
+from argparse import ArgumentParser
 from time import time
-from sys import argv
 
 from ntfs_exception import FileNotFoundException
 from ntfs_parser import NTFSParser
 
-start = time()
 
+def main(args):
+    timer, verbose = False, False
 
-def main():
-    if len(argv) != 2:
-        print('This program will search for a file in file system and print it\'s contents!\n'
-              'Usage: {0} filename_to_search'.format(argv[0]))
-        exit(1)
+    filename = args['file']
 
-    filename = argv[1]
+    if args['time'] is not None:
+        timer = True
+        start = time()
 
-    print('[] Searching for file {0}...'.format(filename))
-    parser = NTFSParser()
+    if args['verbose'] is not None:
+        verbose = True
+        print('[] Searching for file {0}...'.format(filename))
 
     try:
-        ret_val = parser.find_file(filename)
+        ntfs_parser = NTFSParser()
+        ret_val = ntfs_parser.find_file(filename)
 
-        print('[] Found it!\n{0}\'s contents:\n'.format(filename))
+        if verbose:
+            print('[] Found it!\n{0}\'s contents:\n'.format(filename))
+
         print(ret_val, end='\n\n')
     except FileNotFoundException:
-        print('[] {0} file not found!'.format(filename), end='\n\n')
+        if verbose:
+            print('[] {0} file not found!'.format(filename), end='\n\n')
 
-    print('[] Parser finished execution, runtime -> {0}s...'.format(time() - start))
+    if timer:
+        print('[] Parser finished execution, runtime -> {0}s...'.format(time() - start))
 
 
 if __name__ == "__main__":
-    main()
+    parser = ArgumentParser(description='This is an NTFS Parser, give it a filename (not a path) '
+                                        'and the program will print it\'s contents!')
+
+    parser.add_argument('-f', '--file', help='The filename you want the parser to find', required=True)
+    parser.add_argument('-d', '--disk', help='The disk you want the parser to search in.'
+                                             ' Example: --disk=c', required=True)
+
+    parser.add_argument('-t', '--time', help='Add runtime measurement', required=False)
+    parser.add_argument('-v', '--verbose', help='Adding some prints', required=False)
+
+    arguments = vars(parser.parse_args())
+    main(arguments)
